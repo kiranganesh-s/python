40ffe52e8a4d1f720066f9b9a95b7ac7da5c39ce
kiranganesh-s <85781271+kiranganesh-s@users.noreply.github.com>
2021-06-15 20:07:39 +0530

    Add files via upload

---
+++ day 4 python.py
@@ -0,0 +1,27 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=b=c=100
+>>> #devide a by 10
+>>> divi = a/10
+>>> print(int(divi))
+10
+>>> #multiply by 50
+>>> mul =b*50
+>>> print(mul)
+5000
+>>> # add c value by 60
+>>> add =c+60
+>>> print(add)
+160
+>>> # create string variables (a,b) of 5 charecters and replace the 3rd charecter with g
+>>> var ="abcde"
+>>> x=var.replace(var[2],'G')
+>>> print(x)
+abGde
+>>> #create two values (a,b) of int , float data type and convert the vice versa
+>>> a,b = 30,40.5
+>>> A=float(a)
+>>> B=int(b)
+>>> print(A,B)
+30.0 40
+>>> 
