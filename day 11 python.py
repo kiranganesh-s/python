241d79c2ba17badf1c391c69158b4184b6ff1b21
kiranganesh-s <85781271+kiranganesh-s@users.noreply.github.com>
2021-06-25 09:26:04 +0530

    Add files via upload

---
+++ day 11 python.py
@@ -0,0 +1,113 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #1
+>>> import random
+>>> for i in range (1,101):
+	print(random.randint(i,i+1))
+
+	
+2
+2
+4
+4
+6
+6
+7
+8
+9
+10
+11
+13
+13
+15
+15
+16
+18
+19
+20
+21
+21
+23
+23
+25
+26
+27
+27
+28
+29
+31
+31
+33
+34
+35
+36
+36
+38
+39
+40
+41
+41
+42
+44
+44
+45
+47
+48
+49
+49
+50
+51
+53
+53
+54
+55
+57
+57
+58
+59
+61
+62
+63
+64
+64
+66
+66
+68
+68
+69
+71
+72
+73
+74
+74
+76
+77
+77
+79
+80
+81
+81
+83
+83
+85
+86
+86
+88
+88
+90
+91
+91
+93
+93
+94
+96
+96
+98
+98
+99
+101
+>>> #2
+>>> import sys
+>>> print(sys.path)
+['', 'C:\\Users\\nchuk\\AppData\\Local\\Programs\\Python\\Python39\\Lib\\idlelib', 'C:\\Users\\nchuk\\AppData\\Local\\Programs\\Python\\Python39\\python39.zip', 'C:\\Users\\nchuk\\AppData\\Local\\Programs\\Python\\Python39\\DLLs', 'C:\\Users\\nchuk\\AppData\\Local\\Programs\\Python\\Python39\\lib', 'C:\\Users\\nchuk\\AppData\\Local\\Programs\\Python\\Python39', 'C:\\Users\\nchuk\\AppData\\Local\\Programs\\Python\\Python39\\lib\\site-packages']
+>>> 
